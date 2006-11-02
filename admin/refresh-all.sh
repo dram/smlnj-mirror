732ed5715542da43b5fbd8305ccb55ebcf9614f4
blume <blume@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2006-11-02 18:02:24 +0000

    added two scripts for doing global stats and updates
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@2153 d172b47e-f5d9-4df3-ab46-1c4da947e531

---
+++ admin/refresh-all.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ge 1 ] ; then
+    cd $1
+fi
+
+gf=svn://smlnj-gforge.cs.uchicago.edu/smlnj
+
+here=`pwd`
+
+for tree in config base smlnj-lib MLRISC ml-yacc \
+            ckit cml eXene smlnj-c lexgen ml-burg ml-lex heap2asm nlffi
+do
+  if [ -d $tree -a -d $tree/.svn ] ; then
+      echo Refreshing ${tree}...
+      cd $tree
+      svn update
+      cd $here
+  fi
+done
