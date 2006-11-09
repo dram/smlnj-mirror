4e99535ba873f0c78d46b10293a37b6e160c85b5
blume <blume@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2006-11-09 21:30:25 +0000

    created new script for taking release snapshots in the repository
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@2216 d172b47e-f5d9-4df3-ab46-1c4da947e531

---
+++ admin/make-release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $# -lt 1 ] ; then
+    echo Usage: $0 '<release-number>'
+    exit 1
+fi
+
+relno=$1
+
+gf=svn://smlnj-gforge.cs.uchicago.edu/smlnj
+
+here=`pwd`
+
+for tree in config sml smlnj-lib MLRISC ml-yacc trace-debug-profile pgraph \
+            ckit cml eXene smlnj-c lexgen ml-burg ml-lex heap2asm nlffi
+do
+  svn cp $gf/$tree/trunk $gf/$tree/releases/release-$relno -m "Release $relno"
+done
