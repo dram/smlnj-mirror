e87db1ce031e90541ef7e9da264b37e66718df1c
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2013-07-12 21:28:08 +0000

    working on Mac OS X distribution
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@3842 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- osx-dist/build-pkg.sh
+++ osx-dist/build-pkg.sh
@@ -5,15 +5,60 @@
 
 # FIXME: check for VERSION argument!!!
 VERSION=$1
+CONFIGURL=http://smlnj.cs.uchicago.edu/dist/working/$VERSION/config.tgz
+DISTROOT=smlnj.dst
+ROOT=$(pwd)
+
+if [ -d $ROOT ] ; then
+  echo "please remove $ROOT first"
+  exit 1
+fi
+mkdir $ROOT
+cd $ROOT
+
+# first we need to download and unbundle the config directory for the release
+#
+curl -O $CONFIGURL
+tar -xzf config.tgz
+
+# check that the version numbers match
+#
+CONFIG_VERSION=$(cat config/version)
+if [ x"$VERSION" != x"$CONFIG_VERSION" ] ; then
+  echo "version in config/version is $CONFIG_VERSION"
+  exit 1
+fi
+
+# build the distribution (note that this assumes that config/targets is what we want!)
+#
+config/install.sh
+
+# get the other files to include in the distribution
+#
+cp -p $ROOT/components/license.html .
+svn export https://smlnj-gforge.cs.uchicago.edu/svn/smlnj/sml/trunk/READMES/$VERSION-README.html
+
+# cleanup
+#
+rm *tgz
+
+# back up to the root
+#
+cd $ROOT
 
 # patch distribution file for version
 #
-sed -e "s/VERSION/$VERSION/g" distribution_xml.in > distribution.xml
+sed -e "s/VERSION/$VERSION/g" components/distribution_xml.in > distribution.xml
 
 # build package
 #
-pkgbuild --scripts smlnj.dst/config/MacResources/ --install-location /usr/local/smlnj --root smlnj.dst/ smlnj.pkg
+pkgbuild --scripts components/scripts/ --install-location /usr/local/smlnj --root $DISTROOT smlnj.pkg
 
 # build distribution package
 #
-productbuild --sign "Mac Developer: Rich Manalang (3U78U4KMEF)" --distribution ./distribution.xml --package-path . ./smlnj-x86-$VERSION.pkg
+productbuild --sign "Mac Developer: Rich Manalang (3U78U4KMEF)" \
+  --distribution distribution.xml --package-path . ./smlnj-x86-$VERSION.pkg
+
+# cleanup
+#
+#rm distribution.xml
