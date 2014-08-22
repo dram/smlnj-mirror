96492cdd69632c88b9b7d0d49846c8912a0b471e
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2014-08-22 12:54:59 +0000

    add "doc" to the make-release.sh script
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@3924 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- admin/make-release.sh
+++ admin/make-release.sh
@@ -12,7 +12,7 @@ gf=https://smlnj-gforge.cs.uchicago.edu/svn/smlnj
 here=`pwd`
 
 for tree in config sml smlnj-lib MLRISC ml-yacc trace-debug-profile pgraph \
-            ckit cml eXene smlnj-c ml-burg ml-lex heap2asm nlffi
+            ckit cml eXene smlnj-c ml-burg ml-lex heap2asm nlffi doc
 do
   svn cp $gf/$tree/trunk $gf/$tree/releases/release-$relno -m "Release $relno"
 done
