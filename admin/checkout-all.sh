311cd7571173d85f96970ef44c412031f6ba5ae7
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2017-09-20 21:57:21 +0000

    add revision/release options to checkout-all.sh
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@4444 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- admin/checkout-all.sh
+++ admin/checkout-all.sh
@@ -1,16 +1,61 @@
 #!/bin/sh
+#
+# COPYRIGHT (c) 2017 The Fellowship of SML/NJ (http://www.smlnj.org)
+# All rights reserved.
+#
+# Script to checkout a fresh copy of the SML/NJ sources.
+#
+# Usage:
+#	admin/checkout-all.sh [options] [dir]
+#
+# The options are
+#
+#	-e, --export	-- use "svn export" instead of "svn checkout"
+#	-r ARG		-- use ARG as the checkout revision
+#	--release ARG	-- checkout a release that was created with make-release.sh
+#
 
 command=checkout
+revision=""
+release=""
 
-if [ $# -ge 1 ] ; then
-    case $1 in
-	--export|-e)
-	    command=export
-	    shift
-	    ;;
-	*)
-	    ;;
-    esac
+function usage {
+  echo "usage: checkout-all.sh [-e | --export] [-r ARG | --revision ARG] [--release ARG] [dir]"
+  exit $1
+}
+
+while [ $# -ge 1 ] ; do
+  case $1 in
+    --export|-e)
+	command=export
+    ;;
+    --revision|-r)
+	shift
+	if [ $# -ge 1 ] ; then
+	  revision="-r $1"
+	else
+	  usage 1
+	fi
+    ;;
+    --release)
+	shift
+	if [ $# -ge 1 ] ; then
+	  release="releases/release-$1"
+	else
+	  usage 1
+	fi
+    ;;
+    --help|-h) usage 0
+    ;;
+    -*) usage 1
+    ;;
+  esac
+  shift
+done
+
+if [ x"$release" != x -a x"$revision" != x ] ; then
+  echo "checkout-all.sh: cannot specify both a release and revision"
+  exit 1
 fi
 
 if [ $# -ge 1 ] ; then
@@ -18,12 +63,18 @@ if [ $# -ge 1 ] ; then
     cd $1
 fi
 
+# default release is the trunk
+#
+if [ x"$release" = x ] ; then
+  release="trunk"
+fi
+
 gf=https://smlnj-gforge.cs.uchicago.edu/svn
 smlnj=$gf/smlnj
 
 # checkout source target
 checkout(){
-    source=$1/trunk
+    source=$1/$release
     target=$2
     if [ ! -d $target ] ; then
 	echo Checking out $source as $target
@@ -59,9 +110,12 @@ smlnj ml-lex
 smlnj heap2asm
 smlnj nlffi
 
+smlnj doc
+
+# the following do not get tagged releases, so we fallback to regular
+# behavior
+release="trunk"
+revision=""
 checkout $gf/asdl asdl
 checkout $gf/ml-lpt ml-lpt
 checkout $gf/smldoc smldoc
-
-smlnj doc
-
