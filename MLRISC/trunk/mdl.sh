5214c7ea0166618dd26d29e13e17b6b4b43db5aa
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2019-06-21 02:51:00 +0000

    add error checking to mdl.sh
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@5507 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- MLRISC/trunk/mdl.sh
+++ MLRISC/trunk/mdl.sh
@@ -8,13 +8,23 @@
 # usage: mdl.sh <file>
 #
 
+if [ $# -ne 1 ] ; then
+  echo "usage: mdl.sh <file>"
+  exit 1
+fi
 TARGET=$1
+
+src="$TARGET/$TARGET.mdl"
+
+if [ ! -r "$src" ] ; then
+  echo "mdl.sh: unknown target"
+  exit 1
+fi
 
 sml <<XXXX
 val _ = List.app (fn f => #set(CM.Anchor.anchor f) (SOME "cm")) [
 	  "Control.cm", "Lib.cm", "Graphs.cm", "MLRISC.cm", "MLTREE.cm"
 	];
 CM.make "Tools/MDL/sources.cm";
-fun gen f = MDLGen.gen(f ^ "/" ^ f ^ ".mdl");
-gen "$TARGET"
+MDLGen.gen "$src";
 XXXX
