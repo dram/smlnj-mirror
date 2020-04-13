3334157eb2f4295275722948e914f299bb9228ad
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2020-04-13 19:24:12 +0000

    switch from link: to xref:
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@6183 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- smlnj-lib/trunk/NewDoc/src/scripts/prepare-lib.sh
+++ smlnj-lib/trunk/NewDoc/src/scripts/prepare-lib.sh
@@ -48,7 +48,7 @@ while read -r line ; do
       ;;
   esac
   mod_adoc="$d/$stem.adoc"
-  link="link:$stem.html"
+  link="xref:$stem.adoc"
   ## add the module to the library file
   echo $link"[\`"$kw $name"\`]::" >> $lib_adoc
   echo "  something" >> $lib_adoc
@@ -56,7 +56,28 @@ while read -r line ; do
   ## create the placeholder for the module
   sed -e "s/@DIR@/$d/" -e "s/@LIBRARY@/$lib/" -e "s/@NAME@/$name/" $template > $mod_adoc
 done < $d/MODULES
+## copy the middle part
+sed -e "s/@DIR@/$d/" -e "s/@LIBRARY@/$lib/" Templates/lib-mid.adoc >> $lib_adoc
+## add the module entries for the PDF version
+while read -r line ; do
+  name=$(echo $line | sed -e 's/structure //' -e 's/signature //' -e 's/functor //')
+  case $line in
+    signature*)
+      echo "include::sig-$name.adoc[]" >> $lib_adoc
+      echo "" >> $lib_adoc
+      ;;
+    structure*)
+      echo "include::str-$name.adoc[]" >> $lib_adoc
+      echo "" >> $lib_adoc
+      ;;
+    functor*)
+      echo "include::fun-$name.adoc[]" >> $lib_adoc
+      echo "" >> $lib_adoc
+      ;;
+  esac
+done < $d/MODULES
 ## copy the footer
 sed -e "s/@DIR@/$d/" -e "s/@LIBRARY@/$lib/" Templates/lib-foot.adoc >> $lib_adoc
+
 
 exit 0
