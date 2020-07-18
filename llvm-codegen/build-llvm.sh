6fbbe65b2e45bed16f1b8156afe9b614d7f41054
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2020-07-18 14:42:11 +0000

    improvements to build-llvm.sh script
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@6570 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- llvm-codegen/build-llvm.sh
+++ llvm-codegen/build-llvm.sh
@@ -5,18 +5,87 @@
 #
 # Script to configure and build the LLVM sources.
 #
-# usage: build-llvm.sh
+# usage: build-llvm.sh [--force] [-np <n>] [--llvm-src <path>] [--build-dir <path>]
+#
+#       --help                  Generate help message
+#
+#       --force                 Remove any existing build directory before
+#                               configuration
+#
+#       --np <n>                Specify the number of cores to use in the build
+#
+#       --llvm-src <path>       Specify path to LLVM source tree relative to the
+#                               llvm-codegen directory [default: llvm-10.0.0.src]
+#
+#       --build-dir <path>      Specify name of build directory relative to the
+#                               llvm-codegen directory [default: llvm-build]
 #
 
+FORCE=no
 LLVM_SRC=llvm-10.0.0.src
 LLVM_BUILD=llvm-build
 NPROCS=4
 
-case `uname -s`
+# system specific defaults
+#
+case `uname -s` in
   Darwin)
     NPROCS=$(sysctl -n hw.physicalcpu)
   ;;
 esac
+
+usage() {
+  echo "usage: build-llvm.sh [--force] [-np <n>] [--llvm-src <path>] [--build-dir <path>]"
+  echo ""
+  echo "    --help              Generate help message"
+  echo ""
+  echo "    --force             Remove any existing build directory before"
+  echo "                        configuration"
+  echo ""
+  echo "    --np <n>            Specify the number of cores to use in the build"
+  echo "                        [default: $NPROCS]"
+  echo ""
+  echo "    --llvm-src <path>   Specify path to LLVM source tree relative to the"
+  echo "                        llvm-codegen directory [default: $LLVM_SRC]"
+  echo ""
+  echo "    --build-dir <path>  Specify name of build directory relative to the"
+  echo "                        llvm-codegen directory [default: $LLVM_BUILD]"
+
+  exit $1
+}
+
+# process command-line arguments
+#
+while [ "$#" != "0" ] ; do
+  arg=$1; shift
+  case $arg in
+    -h) usage 0 ;;
+    --help) usage 0 ;;
+    --force) FORCE=yes ;;
+    --np)
+      if [ $# -ge 1 ] ; then
+	NPROCS=$1
+	shift
+      else
+	usage 1
+      fi ;;
+    --llvm-src)
+      if [ $# -ge 1 ] ; then
+	LLVM_SRC=$1
+	shift
+      else
+	usage 1
+      fi ;;
+    --build-dir)
+      if [ $# -ge 1 ] ; then
+	LLVM_BUILD=$1
+	shift
+      else
+	usage 1
+      fi ;;
+    *) usage 1 ;;
+  esac
+done
 
 CMAKE_DEFS="\
   -DCMAKE_BUILD_TYPE=Release \
@@ -99,10 +168,16 @@ CMAKE_DEFS="\
 "
 
 if [ -d "$LLVM_BUILD" ] ; then
-  echo "$0: $LLVM_BUILD already exists"
-  exit 1
+  if [ $FORCE = yes ] ; then
+    echo  "$0: removing old $LLVM_BUILD"
+    rm -rf $LLVM_BUILD
+  else
+    echo "$0: $LLVM_BUILD already exists (use --force to override)"
+    exit 1
+  fi
 fi
 
+echo "$0: mkdir $LLVM_BUILD"
 mkdir "$LLVM_BUILD"
 cd "$LLVM_BUILD"
 
@@ -110,4 +185,4 @@ echo "$0: configuring build"
 cmake -G "Unix Makefiles" $CMAKE_DEFS "../$LLVM_SRC" || exit 1
 
 echo "$0: building on $NPROCS cores"
-make -j $NPROCS install
+time make -j $NPROCS install
