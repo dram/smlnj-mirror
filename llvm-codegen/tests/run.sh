994f45a416f23d54b1ed1fa124fb379bcac87ae1
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2020-12-28 18:08:21 +0000

    edit script
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@6849 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- llvm-codegen/tests/run.sh
+++ llvm-codegen/tests/run.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #
+# Script for running code generator tests
+#
 # usage: run.sh [ -emit-llvm ] [ -o | -S | -c ] [ files ... ]
+#
+#	--emit-llvm	-- dump the LLVM IR
+#
+# Target options (default is to print the assembler):
+#
+#	-o		-- generate ".o" files for targets
+#	-S		-- generate ".s" files for targets
+#	-c		-- compile to in-memory machine code and then print
+#			   information about the code
 #
 
 ROOT=/Users/jhr/Work/smlnj/sml-llvm/
@@ -8,13 +19,13 @@ ROOT=/Users/jhr/Work/smlnj/sml-llvm/
 SML=$ROOT/bin/sml
 CODEGEN=$ROOT/llvm-codegen/codegen/codegen
 
+# check that the codegen executable exists
+#
 if [ ! -x $CODEGEN ] ; then
   echo "$0: codegen executable is missing"
   exit 1
 fi
 
-
-#
 # Process command line arguments
 #
 TARGET=""
@@ -33,9 +44,13 @@ while [ "$#" != "0" ]; do
   esac
 done
 
+# if individual tests have not been specified, then run all the tests
+#
 if [ x"$FILES" = x ] ; then
   FILES="tst-*sml"
-  echo "files: $FILES"
+  RUN_ALL=yes
+else
+  RUN_ALL=no
 fi
 
 # remove existing files
@@ -58,21 +73,27 @@ for t in $FILES ; do
   fi
 done
 
+#
+# if we ran all of the tests and were generating .s or .o files, then
+# we check and summarize the results
+#
+
+if [ $RUN_ALL = no ] ; then
+  exit 0
+fi
+
 if [ x"$TARGET" = x-o ] ; then
   SUFFIX="o"
 elif [ x"$TARGET" = x-S ] ; then
-  SUFFIX="S"
+  SUFFIX="s"
 else
   exit 0
 fi
 
-#
-# check results
-#
 echo "# summary:"
 for t in $FILES ; do
   stem=$(basename $t .sml)
-  if [ -r $stem.s ] ; then
+  if [ -r $stem.$SUFFIX ] ; then
     STATUS="success"
   elif [ -r $stem.pkl ] ; then
     STATUS="codegen failed!!"
