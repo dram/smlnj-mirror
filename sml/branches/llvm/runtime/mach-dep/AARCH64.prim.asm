825e110e21085e06b08623b8f449d8b562a3cf40
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2021-02-24 16:47:23 +0000

    working on Arm64
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@6952 d172b47e-f5d9-4df3-ab46-1c4da947e531

--- sml/branches/llvm/runtime/mach-dep/AARCH64.prim.asm
+++ sml/branches/llvm/runtime/mach-dep/AARCH64.prim.asm
@@ -13,8 +13,7 @@
 #include "mlstate-offsets.h"	/** this file is generated **/
 #include "ml-limits.h"
 
-/*
- * AARCH64 register usage and C function calling conventions:
+/* AArch64 register usage and C function calling conventions:
  *
  *	r0-r7		-- argument/result registers
  *	r8		-- indirect result location register
@@ -23,9 +22,41 @@
  *	r30		-- link register
  *	sp		-- stack pointer (r31)
  *
- *	r9,r15		-- temporary registers
+ *	r9-r15		-- temporary registers
  *	r16,r17		-- intra-procedure-call temporaries (IP0,IP1)
  *	r18		-- reserved for platform-specific use
  *
  * The stack pointer must be 16-byte aligned.
  */
+
+/* SML Register conventions for AArch64:
+ *
+ *	alloc ptr
+ *	limit ptr
+ *	store ptr
+ *	exn ptr
+ *	var ptr
+ *	GC link
+ *	base ptr
+ *	std link
+ *	std clos
+ *	std arg
+ *	std cont
+ *	misc0
+ */
+
+#define		allocptr	x24
+#define 	limitptr	x25
+#define 	storeptr	x26
+#define 	exnptr		x27
+#define 	varptr		x28
+#define		stdlink		x3
+#define 	stdclos		x2
+#define 	stdarg		x0
+#define 	stdcont		x1
+#define		miscreg0	x4
+#define		miscreg1	x5
+#define 	miscreg2	x6
+
+#define		gclink		lr
+
