ea099779663a2882e23e96361a773abcc54df7ef
jhr <jhr@d172b47e-f5d9-4df3-ab46-1c4da947e531>
2021-08-06 16:47:57 +0000

    populating the legacy branch
    git-svn-id: https://smlnj-gforge.cs.uchicago.edu/svn/smlnj@7166 d172b47e-f5d9-4df3-ab46-1c4da947e531

---
+++ legacy/admin/make-release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# "tag" a release snapshot by copying all of the trees into a fresh release tree.
+#
+
+if [ $# -lt 1 ] ; then
+    echo Usage: $0 '<release-number>'
+    exit 1
+fi
+
+relno=$1
+
+gf=https://smlnj-gforge.cs.uchicago.edu/svn/smlnj
+
+here=`pwd`
+
+for tree in config sml smlnj-lib MLRISC ml-yacc trace-debug-profile pgraph \
+            ckit cml eXene smlnj-c ml-burg ml-lex heap2asm nlffi doc
+do
+  echo "tagging $tree..."
+  svn cp $gf/$tree/trunk $gf/$tree/releases/release-$relno -m "Release $relno"
+done
